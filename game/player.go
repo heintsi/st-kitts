0f67de4903b2941c04a8f3653eab52644cd44932
Jaakko <jaakko@turska>
2013-12-13 20:44:51 +0200

    Added better checks to validTurn. New Checker interface created and Check implemented for Player, Action and Turn. Checks have some duplicated code at the moment. It is easy to address with an check creator function.

--- game/player.go
+++ game/player.go
@@ -3,3 +3,15 @@ package game
 type Player struct {
 	PlayerID string
 }
+
+func (p *Player) Check() (invalid []string) {
+	if p == nil {
+		invalid = append(invalid, "Player")
+	} else {
+		switch {
+		case p.PlayerID == "":
+			invalid = append(invalid, "Player.PlayerID")
+		}
+	}
+	return
+}
--- game/turn.go
+++ game/turn.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 )
 
+// Types which are read from user input must be checkcd agaist missing
+// or invalid items. The items are returned in a slice by the Check method.
+type Checker interface {
+	Check() []string
+}
+
 // all fields must be pointers in order to check updated fields
 // when unmarshaling json.
 type Turn struct {
@@ -16,13 +22,25 @@ type Action struct {
 	Type string
 }
 
+func (a *Action) Check() (invalid []string) {
+	if a == nil {
+		invalid = append(invalid, "Action")
+	} else {
+		switch {
+		case a.Type == "":
+			invalid = append(invalid, "Action.Type")
+		}
+	}
+	return
+}
+
 type TurnWriteError struct {
 	// contains items which were not provided when submitting the turn
-	MissingItems string
+	MissingItems []string
 }
 
 func (e *TurnWriteError) Error() string {
-	return fmt.Sprintf("JSON write failed since missing:%s", e.MissingItems)
+	return fmt.Sprintf("JSON write failed since invalid:%v", e.MissingItems)
 }
 
 func (t *Turn) Write(p []byte) (n int, err error) {
@@ -31,29 +49,35 @@ func (t *Turn) Write(p []byte) (n int, err error) {
 	if err != nil {
 		return 0, err
 	}
-	ok, err = valid(t)
+	ok, err = validTurn(t)
 	if ok {
 		n = len(p)
 	}
 	return
 }
 
-// checks if all fields in Turn are ok i.e. not nil.
+// checks if all fields in Turn are ok 
 // returns true and nil on success and false and error on failure
-func valid(t *Turn) (ok bool, err error) {
+func validTurn(t *Turn) (ok bool, err error) {
 	// error message will be appended with strings
 	// []byte is needed since only slices can be appended.
-	var missingItems []byte
-	switch {
-		case t.Player == nil:
-			missingItems = append(missingItems, " Player"...)
-		case t.Action == nil:
-			missingItems = append(missingItems, " Action"...)
-	}
-	if len(missingItems) == 0 {
+	invalidItems := t.Check()
+	if len(invalidItems) == 0 {
 		ok = true
 	} else {
-		err = &TurnWriteError{string(missingItems)}
+		err = &TurnWriteError{invalidItems}
+	}
+	return
+}
+
+// All fields in Turn struct are checkcd. If a field is a Struct,
+// it is checked also.
+func (t *Turn) Check() (invalid []string) {
+	if t == nil {
+		invalid = append(invalid, "Turn")
+	} else {
+		invalid = append(invalid, t.Player.Check()...)
+		invalid = append(invalid, t.Action.Check()...)
 	}
 	return
 }
